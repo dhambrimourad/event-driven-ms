a912212c2c46d36e08bcc36c755c0e9025e3391e
mourad <mourad.dhambri@gmail.com>
2022-05-31 12:29:15 -0400

    first commit

---
+++ product-service/src/test/java/com/devops/estore/product/service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.estore.product.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
